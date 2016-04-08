2b92107ffdca4d305768e5ffdb71e8a87fa5b44f
Arian Pradana <arian.raptor@gmail.com>
2016-04-08 11:16:53 +0700

    Major update

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,24 @@ sass = require('gulp-sass'),
 gutil = require('gulp-util'),
 cssNano = require('gulp-cssnano'),
 concat = require('gulp-concat'),
+plumber = require('gulp-plumber'),
 uglify = require('gulp-uglify'),
 autoprefixer = require('gulp-autoprefixer'),
 browserSync = require('browser-sync'),
-merge = require('merge-stream');
+merge = require('merge-stream'),
+del = require('del');
 
 var config = {
   sassPath: './assets/sass/',
   jsPath: './assets/scripts/',
   publicPath: './public/',
   npmDir: './node_modules/',
-}
+};
 
 gulp.slurped = false;
 
 gulp.task('copy', function() {
-  var basscss = gulp.src(config.npmDir + 'basscss-sass/scss/*')
+  var basscss = gulp.src(config.npmDir + 'basscss-sass/scss/**/*.scss')
   .pipe(gulp.dest(config.sassPath + 'vendor/basscss-sass/'));
 
   var jquery = gulp.src(config.npmDir + 'jquery/dist/jquery.min.js')
@@ -35,20 +37,19 @@ gulp.task('copy', function() {
 });
 
 gulp.task('build-css', function() {
-  return gulp.src([
+  gulp.src([
     config.sassPath + 'app.scss',
   ])
-  .pipe(sass().on('error', function(e){
-    console.log(e);
-  }))
+  .pipe(plumber())
+  .pipe(sass().on('error', gutil.log))
   .pipe(autoprefixer({
     browers: [
       'last 2 version',
       'IE 8',
     ]
   }))
-  .pipe(cssNano())
-  .pipe(concat('all.min.css'))
+  .pipe(cssNano().on('error', gutil.log))
+  .pipe(concat('micro-finance.min.css'))
   .pipe(gulp.dest(config.publicPath + 'css/'))
   .pipe(browserSync.reload({
     stream: true,
@@ -56,11 +57,12 @@ gulp.task('build-css', function() {
 });
 
 gulp.task('build-js', function() {
-  return gulp.src([
+  gulp.src([
     config.jsPath + 'main.js',
   ])
-  .pipe(uglify())
-  .pipe(concat('all.min.js'))
+  .pipe(plumber())
+  .pipe(uglify().on('error', gutil.log))
+  .pipe(concat('micro-finance.min.js'))
   .pipe(gulp.dest(config.publicPath + 'scripts/'))
   .pipe(browserSync.reload({
     stream: true,
@@ -68,19 +70,19 @@ gulp.task('build-js', function() {
 });
 
 gulp.task('watch', function() {
-  if(!gulp.slurped) {
-    gulp.watch("gulpfile.js", ["default"]);
-    gulp.watch(config.jsPath + '*.js', ['build-js']);
-    gulp.watch(config.sassPath + '**/*.scss', ['build-css']);
-    gulp.watch(config.publicPath + "*.html", browserSync.reload);
-    browserSync.init({
-      injectChanges: true,
-      server: "./public",
-      open: false,
-      domain: 'http://localhost:3000',
-    });
-    gulp.slurped = true;
-  }
+  gulp.watch(config.jsPath + '*.js', ['build-js']);
+  gulp.watch(config.sassPath + '**/*.scss', ['build-css']);
+  gulp.watch(config.publicPath + "*.html", browserSync.reload);
+  browserSync.init({
+    injectChanges: true,
+    server: "./public",
+    open: false,
+    domain: 'http://localhost:3000',
+  });
+});
+
+gulp.task('clean', function() {
+  return del([config.publicPath + 'css', config.publicPath + 'scripts', config.sassPath + 'vendor', config.jsPath + 'vendor']);
 });
 
 gulp.task('build', ['copy', 'build-css', 'build-js']);
